dbee1f3ab5d064c0f36d7f2a6961b23e25ce82d9
Kapil Israni <090max@gmail.com>
2020-03-07 23:19:25 +0530

    Added mailing feature

--- js/mail.js
+++ js/mail.js
@@ -1,11 +1,30 @@
-function sendEmail(sender) {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "maxp3500@gmail.com",
-    Password: "Saraswati123",
-    To: "090max@gmail.com",
-    From: "maxp3500@gmail.com",
-    Subject: "",
-    Body: "<email body>"
-  }).then(message => alert("mail sent successfully"));
-}
+window.onload = function() {
+  form = document.forms.contactForm;
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    name = form.contactName.value;
+    email = form.contactEmail.value;
+    subject = form.contactSubject.value;
+    body = form.contactMessage.value;
+    message_body =
+      "Name :" +
+      name +
+      "\n" +
+      "sender's Email :" +
+      email +
+      "\n" +
+      "subject :" +
+      subject +
+      "\n" +
+      "message :" +
+      body;
+
+    Email.send({
+      SecureToken: "5af6ff15-6f21-43ef-9a16-e556895b0d8e",
+      To: "090max@gmail.com",
+      From: "maxp3500@gmail.com",
+      Subject: "Enquiry message kapil",
+      Body: message_body
+    }).then(message => alert("mail sent successfully"));
+  });
+};
